b477e3f252280857ecd0cc46b0d525150bca5e67
David vincent <davidjeremaiah@gmail.com>
2022-07-17 05:34:22 +0100

    demonstration on todo list using the four pillars of OOP

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:note/screens/home.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Favor",
+      home: HomeScreen(),
+    );
+  }
+}
